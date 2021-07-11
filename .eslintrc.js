fdf826225c677ef0058ba60032a01a77dc1cdcab
Dmitrii Samsonov <dsamsonov@microsoft.com>
2021-07-11 14:22:55 +0200

    chore: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'no-undef': 'off',
+    'no-console': ['error', { allow: ['warn', 'error', 'assert'] }],
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: false }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { argsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_', ignoreRestSiblings: true },
+    ],
+    curly: 'error',
+    'import/extensions': [
+      'error',
+      'never',
+      { svg: 'always', 'svg?inline': 'always', json: 'always', 'json5?raw': 'always' },
+    ],
+    'import/no-duplicates': 'error',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-default-export': 'error',
+    'import/newline-after-import': 'error',
+    'no-restricted-imports': ['error', 'lodash'],
+    'no-return-await': 'error',
+    'prefer-rest-params': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      // use <root>/tsconfig.json
+      typescript: {},
+      node: true,
+    },
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        'no-console': 'off',
+      },
+    },
+  ],
+}
